1d17fc80cc8e64200cb48dfb7d4faea70985f4f1
togrul-asgerli <81424132+togrul-asgerli@users.noreply.github.com>
2022-11-05 11:50:09 +0400

    Add files via upload

---
+++ SQL_CONNECTION/SQL_CONNECTION/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace SQL_CONNECTION
+{
+    partial class Sql_Connect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.lbl_message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_connect.Location = new System.Drawing.Point(415, 117);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(144, 66);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "Open";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // lbl_message
+            // 
+            this.lbl_message.AutoSize = true;
+            this.lbl_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_message.Location = new System.Drawing.Point(112, 128);
+            this.lbl_message.Name = "lbl_message";
+            this.lbl_message.Size = new System.Drawing.Size(0, 25);
+            this.lbl_message.TabIndex = 1;
+            // 
+            // Sql_Connect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SQL_CONNECTION.Properties.Resources.nature;
+            this.ClientSize = new System.Drawing.Size(767, 450);
+            this.Controls.Add(this.lbl_message);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Sql_Connect";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Label lbl_message;
+    }
+}
+
---
+++ SQL_CONNECTION/SQL_CONNECTION/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_CONNECTION
+{
+    public partial class Sql_Connect : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source = DESKTOP-SQ9BA76\\SQLEXPRESS; Initial Catalog = Northwind; Integrated Security = True");
+        public Sql_Connect()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+             
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                        btn_connect.Text = "Close";
+                        lbl_message.Text = "Connection open!";
+                        return;
+                    }
+                    
+                            conn.Close();
+                            btn_connect.Text = "Open";
+                            lbl_message.Text = "Connection Closed!";
+                
+           
+            
+        }
+
+    }
+}
